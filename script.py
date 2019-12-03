a2371426bb2300f5f9b9ad21a9dfee58d52f4148
Daniel Xiong <dxiong2000@gmail.com>
2019-12-02 21:35:23 -0800

    lol what have i done

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from subprocess import call
 
 # n is number of commits
 n = int(sys.argv[1])
@@ -11,10 +10,11 @@ print(os.path.dirname(os.path.realpath(__file__)))
 for i in range(n):
 	#call(["echo", "\"temp\"", ">", "temp.txt"])
 	open("temp.txt", 'w').close()
-	call(["git", "add", "temp.txt"])
-	call(["git", "commit", "-m", "\"created file {}".format(i)])
-	call(["git", "push", "origin", "master"])
+	os.system("git add temp.txt")
+	os.system("git commit -m \"created file {}\"".format(i))
+	os.system("git push origin master")
 	os.system("del temp.txt")
-	call(["git", "add", "temp.txt"])
-	call(["git", "commit", "-m", "\"deleted file {}".format(i)])
-	call(["git", "push", "origin", "master"])
+	os.system("git add temp.txt")
+	os.system("git commit -m \"deleted file {}\"".format(i))
+	os.system("git push origin master")
+
